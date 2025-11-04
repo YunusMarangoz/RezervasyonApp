4f7fe22c26a3a4a0cdf0f5d2a4054a71da7cf95e
Yunus <yunusmarangoz@gmail.com>
2025-11-04 22:59:14 +0300

    4-11-2025

--- RezervasyonApp/Areas/Admin/Controllers/MainController.cs
+++ RezervasyonApp/Areas/Admin/Controllers/MainController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace RezervasyonApp.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class MainController : Controller
     {
         public IActionResult Index()
